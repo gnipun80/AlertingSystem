7ebee457ad255475b79391b577c8e869df4cdc91
Prateek <prateek@checkoutmyshit.com>
2024-02-17 23:24:03 +0530

    Minor

--- src/main/java/AlertSystem/service/Alert/TumblingWindow.java
+++ src/main/java/AlertSystem/service/Alert/TumblingWindow.java
@@ -1,5 +1,10 @@
 package AlertSystem.service.Alert;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import AlertSystem.Enums.ConfigType;
@@ -15,10 +20,19 @@ public class TumblingWindow extends AlertConfig{
     @Override
     public boolean process(List<EventInfo> eventList) {
         EventInfo lastEvent = eventList.get(eventList.size() - 1);
-        int lastEventWindowIndex = (int) ((lastEvent.getEventTime() % (24 * 3600)) / windowSizeInSecs), count = 1;
-        for (EventInfo event : eventList) {
-            int currWindowIndex = (int) ((event.getEventTime() % (24 * 3600)) / windowSizeInSecs);
-            if (event != lastEvent && currWindowIndex == lastEventWindowIndex) {
+        Instant instant = Instant.ofEpochSecond(lastEvent.getEventTime());
+        ZonedDateTime dateTime = instant.atZone(ZoneId.of("GMT"));
+        ZonedDateTime startOfDay = dateTime.truncatedTo(ChronoUnit.DAYS);
+        long diffInSeconds = ChronoUnit.SECONDS.between(startOfDay, dateTime);
+        long lastEventWindow = diffInSeconds / windowSizeInSecs;
+        int count = 0;
+        for (int i = eventList.size() - threshold; i < eventList.size(); i++) {
+            EventInfo event = eventList.get(i);
+            instant = Instant.ofEpochSecond(event.getEventTime());
+            dateTime = instant.atZone(ZoneId.of("GMT"));
+            diffInSeconds = ChronoUnit.SECONDS.between(startOfDay, dateTime);
+            long eventWindow = diffInSeconds / windowSizeInSecs;
+            if (eventWindow == lastEventWindow) {
                 count++;
             }
         }
